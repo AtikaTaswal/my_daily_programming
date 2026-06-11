1ae16a3de14cf937095cf662055e5f217c3066c9
Atika Taswal <atikataswal0622@gmail.com>
2026-06-12 02:12:34 +0800

    my first commit

---
+++ Sum_Sub.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,m,a,b,sum,sub;
+    
+    scanf("%d %d",&n,&m);
+
+    sum=n+m;
+    sub=n-m;
+
+printf("The sum is %d and the sub is %d",sum,sub);
+    scanf("%d %d",&a,&b);
+
+    sum=a+b;
+    sub=a-b;
+
+printf("The sum is %d and the sub is %d",sum,sub);
+
+return 0;
+
+}
